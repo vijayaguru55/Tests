3269531cd87e7cb0445c7013aaaeae89d34e7aaa
Vijayaguru S <94888838+vijayaguru55@users.noreply.github.com>
2023-01-25 11:40:00 +0530

    Add files via upload
    another approach

--- CheckChars.java
+++ CheckChars.java
@@ -1,31 +1,30 @@
 package org.Test1;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CheckChars {
     static String checkChars(String s1, String s2){
         String val="";
-        
+        Set<Character> set =  new HashSet<>();
+        for(int i=0;i<s2.length();i++)
+            set.add(s2.charAt(i));
+
         int[] ch = new int[26];
         Arrays.fill(ch, 0);
 
-        if(s2.length()>s1.length())
-            return null;
         
-
-        for(int i=0;i<s2.length();i++){
-            for(int j=0;j<s1.length();j++){
-            char ref = s1.charAt(j);
-            if(s2.charAt(i)==ref){
-                ch[s2.charAt(i)-97]+=1;
-            }
-        }
-                
         
+        for(int j=0;j<s1.length();j++){
+            char origin = s1.charAt(j);
+            if(set.contains(origin))ch[origin-97]+=1;
+    
         }
         for(int i=0;i<s2.length();i++)
-            val+=s2.charAt(i)+":"+ch[s2.charAt(i)-97];
+            val+=s2.charAt(i)+":"+ch[s2.charAt(i)-97]+" ";
 
         return val;
     }
